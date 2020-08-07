614ac878ae7eef30095b7a87ac79f87ccdb34108
David <b4rbab4rba@gmail.com>
2020-08-07 18:19:35 -0300

    Atualização de  API Security

---
+++ farmacia/src/main/java/com/farmacia/farmacia/seguranca/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.farmacia.farmacia.seguranca;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.farmacia.farmacia.model.Usuario;
+import com.farmacia.farmacia.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Optional<Usuario> user = userRepository.findByUsuario(username); user.orElseThrow(() -> new UsernameNotFoundException(username + " not found.")); 
+		return user.map(UserDetailsImpl::new).get(); } } 
+
